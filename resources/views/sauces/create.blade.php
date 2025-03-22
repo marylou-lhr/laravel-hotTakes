881bd15bac597c28169318ee5234fe2e8499b276
Marylou Lohier <mlohier001@iutbayonne.univ-pau.fr>
2025-03-22 16:23:18 +0100

    Corrections et ajout de create.blade.php et de edit.blade.php non testable

--- resources/views/sauces/create.blade.php
+++ resources/views/sauces/create.blade.php
@@ -1,5 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <h1 class="mb-4">Add a new sauce</h1>
+    <form action="{{ route('sauces.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <!-- Name -->
+            <div class="mb-3">
+                <label for="name" class="form-label">{{ __('Name') }}</label>
+                <input type="text" name="name" id="name" class="form-control" required>
+            </div>
+            <!-- Manufacturer -->
+            <div class="mb-3">
+                <label for="manufacturer" class="form-label">{{ __('Manufacturer') }}</label>
+                <input type="text" name="manufacturer" id="manufacturer" class="form-control" required>
+            </div>
+            <!-- Description -->
+            <div class="mb-3">
+                <label for="description" class="form-label">{{ __('Description') }}</label>
+                <textarea name="description" id="description" class="form-control" rows="3" required></textarea>
+            </div>
+            <!-- Main Pepper -->
+            <div class="mb-3">
+                <label for="mainPepper" class="form-label">{{ __('Main pepper') }}</label>
+                <input type="text" name="mainPepper" id="mainPepper" class="form-control" required>
+            </div>
+            <!-- Image -->
+            <div class="mb-3">
+                <label for="imageUrl" class="form-label">{{ __('Image') }}</label>
+                <input type="file" name="imageUrl" id="imageUrl" class="form-control" accept="image/*"/>
+            <!-- Heat -->
+            <div class="mt-3 mb-3">
+                <label for="heat" class="form-label">{{ __('Heat') }}</label><br>
+                <input type="range" name="heat" id="heat" min="1" max="10" required>
+            </div>
 
+            <!-- Hidden attributes -->
+            <input type="hidden" name="likes" value="0">
+            <input type="hidden" name="dislikes" value="0">
+
+            <!-- Submit -->
+            <button type="submit" class="btn btn-secondary">SUBMIT</button>
+    </form>
+</div>
 @endsection
--- resources/views/sauces/index.blade.php
+++ resources/views/sauces/index.blade.php
@@ -9,7 +9,7 @@
                 <div class="text-align-center">
                     <a href="{{ route('sauces.show', $sauce->id) }}" class="stretched-link text-decoration-none d-flex flex-column" style="height: 100%;">
                         <div class="card-body d-flex flex-column text-align-center align-items-center justify-content-between  flex-grow-1">
-                            <img src="{{ asset($sauce->imageUrl) }}" class="card-img-top" alt="{{ $sauce->name }}">
+                            <img src="{{ asset($sauce->imageUrl) }}" class="card-img-top">
                             <div class="d-flex flex-column text-align-center align-items-center justify-content-between text-dark">
                                 <h6 class="text-center text-uppercase fw-bold">{{ $sauce->name }}</h6>
                                 <small>Heat : {{ $sauce->heat }}/10</small>
